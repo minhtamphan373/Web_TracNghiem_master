8ede52ea74e2144cee7790f8cba896353e3c03ee
minhtamphan373 <minhtamphan373@gmail.com>
2023-04-07 20:07:49 +0700

    Web_TracNghiem_master

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, question,
+let questions = [
+    {
+        numb:1,
+        question:"Những giai cấp mới ra đời ở Việt Nam sau Chiến tranh thế giới thứ nhất là",
+        answer:"Tư sản và tiểu tư sản",
+        options:[
+            "Tư sản và tiểu tư sản",
+            "Công nhân và tư sản",
+            "Công nhân và tiểu tư sản",
+            "Địa chủ và tư sản dân tộc",
+        ]
+    },
+    {
+        numb:2,
+        question:"Theo thỏa thuận tại Hội nghị Pốtxđam (1945), quân đội Trung Hoa Dân quốc vào Việt Nam giải giáp quân đội Nhật từ",
+        answer:"Vĩ tuyến 16 trở ra Bắc",
+        options:[
+            "Vĩ tuyến 17 trở vào Nam",
+            "Vĩ tuyến 16 trở ra Bắc",
+            "Vĩ tuyến 16 trở vào Nam",
+            "Vĩ tuyến 17 trở ra Bắc",
+        ]
+    },
+    {
+        numb:3,
+        question:"Trong cuộc khai thác thuộc địa lần thứ hai ở Đông Dương (1919 - 1929), thực dân Pháp đầu tư vốn nhiều nhất vào ngành kinh tế nào?",
+        answer:"Nông nghiệp",
+        options:[
+            "Thương nghiệp",
+            "Công nghiệp",
+            "Thủ công nghiệp",
+            "Nông nghiệp",
+        ]
+    },
+    {
+        numb:4,
+        question:"Hội nghị lần thứ nhất Ban Chấp hành Trung ương lâm thời Đảng Cộng sản Việt Nam (10/1930) quyết định đổi tên Đảng Cộng sản Việt Nam thành",
+        answer:"Đông Dương Cộng sản đảng",
+        options:[
+            "Đảng Cộng sản Đông Dương",
+            "Đông Dương Cộng sản đảng",
+            "Đảng Dân chủ Việt Nam",
+            "Đảng Lao động Việt Nam",
+        ]
+    },
+    {
+        numb:5,
+        question:"Năm 1929, những tổ chức cộng sản nào xuất hiện ở Việt Nam?",
+        answer:"Đông Dương Cộng sản đảng, An Nam Cộng sản Đảng, Đông Dương Cộng sản liên đoàn",
+        options:[
+            "Việt Nam Quốc dân đảng, An Nam Cộng sản đảng, Đông Dương Cộng đảng",
+            "Đông Dương Cộng sản đảng, An Nam Cộng sản Đảng, Đông Dương Cộng sản liên đoàn",
+            "Hội Việt Nam Cách mạng Thanh niên, An Nam Cộng sản đảng, Đông Dương Cộng sản đảng",
+            "Tân Việt Cách mạng đảng, An Nam Cộng sản đảng, Đông Dương Cộng sản đảng",
+        ]
+    },
+];
