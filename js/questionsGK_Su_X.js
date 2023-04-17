8b71d44d85ff323845af07c796606293cad920f9
B-baoha-H <baoha2312@gmail.com>
2023-04-17 20:15:52 +0700

    commit de LS GK THPT X+Y

---
+++ js/questionsGK_Su_X.js
@@ -0,0 +1,127 @@
+//creating an array and passing the number, question,
+let questions = [
+    {
+        numb:1,
+        question:"Sự kiện lịch sử thế giới nào sau đây có ảnh hưởng đến cách mạng Việt Nam thời kì 1919-1930?",
+        answer:"Nước Nga Xô việt được thành lập.",
+        options:[
+            "Hiệp ước an ninh Mỹ-Nhật được kí kết.",
+            "Liên hợp quốc được thành lập.",
+            "Xu thế toàn cầu hóa xuất hiện.",
+            "Nước Nga Xô việt được thành lập.",
+        ]
+    },
+    {
+        numb:2,
+        question:"Lực lượng vũ trang được xây dựng ở Việt Nam trong giai đoạn 1939-1945 là",
+        answer:"Trung đội Cứu quốc quân III.",
+        options:[
+            "Việt Nam Quang phục hội.",
+            "Trung đội Cứu quốc quân III.",
+            "Mặt trận Dân chủ Đông Dương.",
+            "Mặt trận Tổ quốc Việt Nam.",
+        ]
+    },
+    {
+        numb:3,
+        question:"Lực lượng vũ trang được xây dựng ở Việt Nam trong giai đoạn 1939-1945 là",
+        answer:"Việt Nam Giải phóng quân.",
+        options:[
+            "Mặt trận Dân chủ Đông Dương.",
+            "Việt Nam Giải phóng quân.",
+            "Việt Nam Quang phục hội.",
+            "Mặt trận Tổ quốc Việt Nam.",
+        ]
+    },
+    {
+        numb:4,
+        question:"Lực lượng vũ trang được xây dựng ở Việt Nam trong giai đoạn 1939-1945 là",
+        answer:"Trung đội Cứu quốc quân I.",
+        options:[
+            "Trung đội Cứu quốc quân I.",
+            "Mặt trận Dân chủ Đông Dương.",
+            "Việt Nam Quang phục hội.",
+            "Mặt trận Tổ quốc Việt Nam.",
+        ]
+    },
+    {
+        numb:5,
+        question:"Sau Chiến tranh thế giới thứ hai, một trong những quốc gia ở châu Phi giành được độc lập là",
+        answer:"Ănggôla.",
+        options:[
+            "Nhật Bản.",
+            "Ấn Độ.",
+            "Ănggôla.",
+            "Trung Quốc.",
+        ]
+    },
+    {
+        numb:6,
+        question:"Sự kiện nào diễn ra trong thời kì Chiến tranh lạnh (1947-1989)?",
+        answer:"Tổ chức Hiệp ước Vácsava được thành lập.",
+        options:[
+            "Chiến tranh thế giới thứ nhất kết thúc.",
+            "Chiến tranh thế giới thứ hai bùng nổ.",
+            "Tổ chức Hiệp ước Vácsava được thành lập.",
+            "Hệ thống Vécxai-Oasinhtơn được thiết lập.",
+        ]
+    },
+    {
+        numb:7,
+        question:"Phong trào Cần Vương cuối thế kỉ XIX ở Việt Nam bùng nổ sau khi",
+        answer:"Triều đình Huế kí với Pháp bản Hiệp ước Patơnốt.",
+        options:[
+            "Việt Nam Quốc dân đảng hoàn toàn tan rã.",
+            "Triều đình Huế kí với Pháp bản Hiệp ước Patơnốt.",
+            "Đông Dương Cộng sản liên đoàn được thành lập.",
+            "Khởi nghĩa Yên Bái hoàn toàn thất bại.",
+        ]
+    },
+    {
+        numb:8,
+        question:"Nội dung nào sau đây phản ánh không đúng về chiến thắng Điện Biên Phủ năm 1954 của Việt Nam?",
+        answer:"Đánh thắng chiến lược Việt Nam hóa chiến tranh của Mĩ.",
+        options:[
+            "Giáng đòn quyết định vào ý chí xâm lược của thực dân Pháp.",
+            "Làm phá sản hoàn toàn kế hoạch Nava của thực dân Pháp.",
+            "Đánh thắng chiến lược Việt Nam hóa chiến tranh của Mĩ.",
+            "Làm xoay chuyển cục diện chiến tranh ở Đông Dương.",
+        ]
+    },
+    {
+        numb:9,
+        question:"Nội dung nào sau đây là một trong những yếu tố dẫn đến thắng lợi của phong trào giải phóng dân tộc ở châu Phi sau Chiến tranh thế giới thứ hai?",
+        answer:"Sự trưởng thành của ý thức dân tộc.",
+        options:[
+            "Sự trưởng thành của ý thức dân tộc.",
+            "Nhận được viện trợ của tất cả các nước châu Âu.",
+            "Nhận được viện trợ của các nước Đông Dương.",
+            "Có sự viện trợ của tất cả các nước Mĩ Latinh.",
+        ]
+    },
+    {
+        numb:10,
+        question:"Nội dung nào sau đây là nguồn gốc của cuộc cách mạng khoa học-kĩ thuật diễn ra từ những năm 40 của thế kỉ XX?",
+        answer:"Những đòi hỏi của sản xuất.",
+        options:[
+            "Mĩ thiết lập trật tự đơn cực.",
+            "Trật tự đa cực được thiết lập.",
+            "Những đòi hỏi của sản xuất.",
+            "Trật tự hại cực Ianta sụp đổ.",
+        ]
+    },
+    /*
+    ----
+    {
+        numb:number,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            "",
+        ]
+    },
+    */
+];
--- js/questionsGK_Su_Y.js
+++ js/questionsGK_Su_Y.js
@@ -55,4 +55,73 @@ let questions = [
             "Tân Việt Cách mạng đảng, An Nam Cộng sản đảng, Đông Dương Cộng sản đảng",
         ]
     },
+    {
+        numb:6,
+        question:"Sự kiện nào sau đây diễn ra trong thời kì Chiến tranh lạnh (1947-1989)?",
+        answer:"Tổ chức Hiệp ước Vácsava được thành lập.",
+        options:[
+            "Chiến tranh thế giới thứ nhất kết thúc.",
+            "Chiến tranh thế giới thứ hai bùng nổ.",
+            "Tổ chức Hiệp ước Vácsava được thành lập.",
+            "Hệ thống Vécxai-Oasinhtơn được thiết lập.",
+        ]
+    },
+    {
+        numb:7,
+        question:" Theo quyết định của Hội nghị Ianta (tháng 2-1945), quân đội Liên Xô chiếm đóng khu vực nào sau đây?",
+        answer:"Đông Âu.",
+        options:[
+            "Đông Âu.",
+            "Tây Âu.",
+            "Tây Béclin.",
+            "Tây Đức",
+        ]
+    },
+    {
+        numb:8,
+        question:"Trong chương trình khai thác thuộc địa lần thứ hai ở Đông Dương (1919-1929), thực dân Pháp",
+        answer:"Chỉ phát triển công nghiệp cơ khí.",
+        options:[
+            "Mở rộng diện tích trồng cao su.",
+            "Chỉ phát triển công nghiệp hóa chất.",
+            "Tập trung vào công nghiệp luyện kim.",
+            "Chỉ phát triển công nghiệp cơ khí.",
+        ]
+    },
+    {
+        numb:9,
+        question:"Trong những năm 1976-1986, nhân dân Việt Nam thực hiện một trong những nhiệm vụ nào sau đây?",
+        answer:"Bảo vệ biên giới phía Bắc.",
+        options:[
+            "Xây dựng Hội Liên Việt.",
+            "Bảo vệ biên giới phía Bắc.",
+            "Xây dựng Quỹ độc lập.",
+            "Bầu Quốc hội khóa I.",
+        ]
+    },
+    {
+        numb:10,
+        question:"Về chính trị, trong những năm 1951-1953, ở Việt Nam diễn ra sự kiện nào sau đây?",
+        answer:"Đại hội toàn quốc thống nhất Việt Minh-Liên Việt.",
+        options:[
+            "Chiến dịch giải phóng Huế - Đà Nẵng.",
+            "Quân giải phóng miền Nam ra đời.",
+            "Cuộc Tổng tiến công và nổi dậy Xuân Mậu Thân.",
+            "Đại hội toàn quốc thống nhất Việt Minh-Liên Việt.",
+        ]
+    },
+    /*
+    ----
+    {
+        numb:number,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            "",
+        ]
+    },
+    */
 ];
